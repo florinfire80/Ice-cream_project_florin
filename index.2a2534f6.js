b0ee3c4600dcdf3ef2208d34975611a0f83badc7
florinfire80 <127885092+florinfire80@users.noreply.github.com>
2023-09-28 05:47:39 +0000

    Deploying to gh-pages from @ florinfire80/Ice-cream_project_florin@2b18f33a6b8dad727ed534a8c3c5b57096a78b53 🚀

---
+++ index.2a2534f6.js
@@ -0,0 +1,2 @@
+new Swiper("#swiper-1",{loop:!0,autoplay:{delay:4e3,disableOnInteraction:!1},pagination:{el:"#swiper-1 .swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.2a2534f6.js.map
---
+++ index.fe6caf6b.js
@@ -0,0 +1,2 @@
+function openModal(){var e=document.querySelector("#open-modal"),n=document.querySelector(".modal-area"),o=document.querySelector(".modal-area-box");e.addEventListener("click",(function(){n.style.display="flex"})),n.addEventListener("click",(function(){n.style.display="none"})),o.addEventListener("click",(function(e){e.stopPropagation()}))}openModal();
+//# sourceMappingURL=index.fe6caf6b.js.map
